578722e18a4ba736c97347677e28383e96bddfb1
judahmeek <judah.meek@gmail.com>
2016-02-01 05:36:57 +0000

    Updated views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def title(page_title)
+      content_for(:title) { page_title }
+    end
+    
+    def create_opts(opts)
+      content_for(:create_opts) { opts }
+    end
 end
